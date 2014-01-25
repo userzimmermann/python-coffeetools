10e6d7860b45bcb4b023bfc7494cd368eae3ea5a
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-01-25 10:48:33 +0000

    Better setup.

--- setup.py
+++ setup.py
@@ -1,31 +1,40 @@
-import sys
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+VERSION = open('VERSION').read().strip()
+
+REQUIRES = open('requirements.txt').read().strip().split('\n')
+
 
 setup(
-  name = 'coffeetools',
-  version = '0.1a',
-  description = (
+  name='coffeetools',
+  version=VERSION,
+  description=(
     'Tools for using CoffeeScript with Python.'
     ),
-  author = 'Stefan Zimmermann',
-  author_email = 'zimmermann.code@gmail.com',
-  url = 'http://bitbucket.org/userzimmermann/python-coffeetools',
+  author='Stefan Zimmermann',
+  author_email='zimmermann.code@gmail.com',
+  url='http://bitbucket.org/userzimmermann/python-coffeetools',
 
-  license = 'GPLv3',
+  license='LGPLv3',
 
-  packages = [
+  install_requires=REQUIRES,
+  packages=[
     'coffeetools',
     'coffeetools.jinja',
     ],
 
-  classifiers = [
+  classifiers=[
     'Development Status :: 3 - Alpha',
     'License :: OSI Approved :: GNU General Public License (GPL)',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     ],
-  keywords = [
+  keywords=[
     'coffee', 'tools', 'script', 'coffeescript', 'javascript',
-    'jinja', 'jinja2', 'template', 'tag', 'extension',
+    'jinja', 'jinja2', 'jinjatools', 'template', 'tag', 'extension',
     ],
   )
